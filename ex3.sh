f93bfe84ffed3aa4e49b15012dc12fd94b9a897a
Margineanu Tudor <tudormargineanu55@gmail.com>
2026-01-14 00:55:39 +0200

    Verificare IP folosind functie shell si DNS

--- ex3.sh
+++ ex3.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-cat /etc/hosts | while read ip nume r; do
+check_id() {
+	local host=$1
+	local ip=$2
+	local dns=$3
 	if [[ $ip == '#' ]] ; then
 		exit 0
 	fi
-	[[ -z $nume ]] && continue
-	nsip=$(nslookup $nume 1.1.1.1 | awk '/^Address: / {print $2}' | head -1)
+	[[ -z $host ]] && return 1
+	nsip=$(nslookup $host $dns | awk '/^Address: / {print $2}' | head -1)
 	
-	if [[ $ip != $nsip && $nume != "localhost" && $nume != "ubuntu" ]] ; then
-		echo "Bogus IP for $nume in /etc/hosts!"
+	if [[ $ip != $nsip && $host != "localhost" && $host != "ubuntu" ]] ; then
+		echo "Bogus IP for $host in /etc/hosts!"
 	fi
+}
+
+dns_server="1.1.1.1"
+
+cat /etc/hosts | while read ip nume r; do
+	check_id "$nume" "$ip" "$dns_server"
 	
 done
